d2a7cb59c3a0fe161e454a148c74e8401aa9818d
Salah <farzin2008@gmail.com>
2024-08-31 00:40:18 +0200

    Initial commit: First commit
    This is the base commit of the project
    
    Change-Id: 96998201-ee30-4b5c-8e9d-e6ba66aed360

---
+++ api.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\App;
+
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+$app = new App();
+// TODO A: Improve the readability of this file through refactoring and documentation.
+// TODO B: Clean up the following code so that it's easier to see the different
+// routes and handlers for the API, and simpler to add new ones.
+// TODO C: If there are performance concerns in the current code, please
+// add comments on how you would fix them
+// TODO D: Identify any potential security vulnerabilities in this code.
+// TODO E: Document this code to make it more understandable
+// for other developers.
+
+header( 'Content-Type: application/json' );
+if ( !isset( $_GET['title'] ) && !isset( $_GET['prefixsearch'] ) ) {
+	echo json_encode( [ 'content' => $app->getListOfArticles() ] );
+} elseif ( isset( $_GET['prefixsearch'] ) ) {
+	$list = $app->getListOfArticles();
+	$ma = [];
+	foreach ( $list as $ar ) {
+		if ( strpos( strtolower( $ar ), strtolower( $_GET['prefixsearch'] ) ) === 0 ) {
+			$ma[] = $ar;
+		}
+	}
+	echo json_encode( [ 'content' => $ma ] );
+} else {
+	echo json_encode( [ 'content' => $app->fetch( $_GET ) ] );
+}
